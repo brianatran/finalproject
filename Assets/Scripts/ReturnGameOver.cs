b0fc990868e6515a750992989f1cc380c626585d
tranbriana <brianatran@Brianas-MacBook-Pro.local>
2016-11-30 11:05:12 -0800

    no message

---
+++ Assets/Scripts/ReturnGameOver.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ReturnGameOver : MonoBehaviour {
+
+
+	public void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+			SceneManager.LoadScene ("gameover");
+			SceneManager.UnloadScene ("park");
+		}
+		else if (Input.GetMouseButtonDown (0)) {
+			SceneManager.LoadScene ("gameover");
+			SceneManager.UnloadScene ("pink");
+		}
+		if (Input.GetMouseButtonDown (0)) {
+			SceneManager.LoadScene ("gameover");
+			SceneManager.UnloadScene ("winter");
+		}
+
+	}
+}
